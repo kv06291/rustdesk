0d48abda89ee0198c43cf7f6bdddf645df14369c
kv06291 <kv0629@126.com>
2025-06-27 17:35:06 +0800

    12422

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -74,7 +74,10 @@ lazy_static::lazy_static! {
     pub static ref OVERWRITE_LOCAL_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     	pub static ref HARD_SETTINGS: RwLock<HashMap<String, String>> = {
         let mut map = HashMap::new();
-        map.insert("password".to_string(), option_env!("HARD_SETTINGS_S").to_string());
+        map.insert("password".to_string(), option_env!("HARD_SETTINGS_S"). {
+    Some(key) if !key.is_empty() => key,
+    _ => "",
+    }.to_owned());
         RwLock::new(map)
     };
     pub static ref BUILTIN_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
